d35b3bd22a8bfe018fa83ed2ac8c80a26ae32343
Willian Antunes <willian.lima.antunes@gmail.com>
2021-02-08 16:41:05 -0300

    Converting the Issues List Page part I

--- github-issues-app/src/app/rootReducer.ts
+++ github-issues-app/src/app/rootReducer.ts
@@ -1,8 +1,13 @@
 import { combineReducers } from "@reduxjs/toolkit"
-import issuesDisplayReducer from "../features/issuesDisplay/issuesDisplaySlice"
+
+import issuesDisplayReducer from "features/issuesDisplay/issuesDisplaySlice"
+import repoDetailsReducer from "features/repoSearch/repoDetailsSlice"
+import issuesReducer from "features/issuesList/issuesSlice"
 
 const rootReducer = combineReducers({
-  issuesDisplay: issuesDisplayReducer
+  issuesDisplay: issuesDisplayReducer,
+  repoDetails: repoDetailsReducer,
+  issues: issuesReducer
 })
 
 export type RootState = ReturnType<typeof rootReducer>
--- github-issues-app/src/features/issuesList/IssuesListPage.tsx
+++ github-issues-app/src/features/issuesList/IssuesListPage.tsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux"
 
-import { getIssues, getRepoDetails, IssuesResult } from "api/githubAPI"
+import { getIssues, IssuesResult } from "api/githubAPI"
+
+import { fetchIssuesCount } from "features/repoSearch/repoDetailsSlice"
+import { RootState } from "app/rootReducer"
 
 import { IssuesPageHeader } from "./IssuesPageHeader"
 import { IssuesList } from "./IssuesList"
@@ -15,14 +19,16 @@ interface ILProps {
 }
 
 export const IssuesListPage = ({ org, repo, page = 1, setJumpToPage, showIssueComments }: ILProps) => {
+  const dispatch = useDispatch()
+
   const [issuesResult, setIssues] = useState<IssuesResult>({
     pageLinks: null,
     pageCount: 1,
     issues: []
   })
-  const [numIssues, setNumIssues] = useState<number>(-1)
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [issuesError, setIssuesError] = useState<Error | null>(null)
+  const openIssueCount = useSelector((state: RootState) => state.repoDetails.openIssuesCount)
 
   const { issues, pageCount } = issuesResult
 
@@ -33,13 +39,8 @@ export const IssuesListPage = ({ org, repo, page = 1, setJumpToPage, showIssueCo
         setIssues(issuesResult)
       }
 
-      async function fetchIssueCount() {
-        const repoDetails = await getRepoDetails(org, repo)
-        setNumIssues(repoDetails.open_issues_count)
-      }
-
       try {
-        await Promise.all([fetchIssues(), fetchIssueCount()])
+        await Promise.all([fetchIssues(), dispatch(fetchIssuesCount(org, repo))])
         setIssuesError(null)
       } catch (err) {
         console.error(err)
@@ -52,7 +53,7 @@ export const IssuesListPage = ({ org, repo, page = 1, setJumpToPage, showIssueCo
     setIsLoading(true)
 
     fetchEverything()
-  }, [org, repo, page])
+  }, [org, repo, page, dispatch])
 
   if (issuesError) {
     return (
@@ -78,7 +79,7 @@ export const IssuesListPage = ({ org, repo, page = 1, setJumpToPage, showIssueCo
 
   return (
     <div id="issue-list-page">
-      <IssuesPageHeader openIssuesCount={numIssues} org={org} repo={repo} />
+      <IssuesPageHeader openIssuesCount={openIssueCount} org={org} repo={repo} />
       {renderedList}
       <IssuePagination currentPage={currentPage} pageCount={pageCount} onPageChange={onPageChanged} />
     </div>
