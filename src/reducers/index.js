3b8ceeea70fdbcf32567f299ca6af4421e06de09
Willian Antunes <willian.lima.antunes@gmail.com>
2021-02-08 11:11:56 -0300

    Use the new filters action in the UI

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux"
 // import todos from "./todos"
 import todosReducer from "../features/todos/todosSlice"
-import visibilityFilter from "./visibilityFilter"
+// import visibilityFilter from "./visibilityFilter"
+import visibilityFilterReducer from "../features/filters/filtersSlice"
 
 export default combineReducers({
   // todos,
   todos: todosReducer,
-  visibilityFilter,
+  // visibilityFilter,
+  visibilityFilter: visibilityFilterReducer,
 })
